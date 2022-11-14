edfcba86d57f3022ecb34b65c819db845162dbb8
Victor Chiletto <v@hnn.net.br>
2022-11-14 02:25:55 -0300

    gen-srf: remove an unecessary intermediate collect

--- src/commands/gen_srf.rs
+++ src/commands/gen_srf.rs
@@ -6,17 +6,14 @@ use std::path::Path;
 use walkdir::WalkDir;
 
 pub fn gen_srf(base_path: &Path) {
-    let paths: Vec<_> = WalkDir::new(base_path)
+    let _mods: Vec<_> = WalkDir::new(base_path)
         .max_depth(1)
         .into_iter()
         .skip(1)
+        .par_bridge()
         .filter_map(|e| e.ok())
-        .map(|entry| entry.path().to_owned())
-        .collect();
-
-    let _mods: Vec<_> = paths
-        .par_iter()
-        .map(|path| {
+        .map(|entry| {
+            let path = entry.path();
             let generated_srf = srf::scan_mod(path).unwrap();
 
             let path = path.join("mod.srf");
