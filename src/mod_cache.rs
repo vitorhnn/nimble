dfdf701f239b7dd46c2dd5a559071c32ffd87fdd
Victor Chiletto <v@hnn.net.br>
2022-11-22 07:46:19 -0300

    mod_cache, gen_srf, sync: move responsibility of serde into mod_cache

--- src/mod_cache.rs
+++ src/mod_cache.rs
@@ -1,6 +1,22 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
+use std::path::Path;
+use std::fs::File;
+use std::io::{BufReader, BufWriter};
+use snafu::{Snafu, ResultExt};
 use crate::md5_digest::Md5Digest;
+
+#[derive(Debug, Snafu)]
+pub enum Error {
+    #[snafu(display("failed to create cache file: {}", source))]
+    FileCreation { source: std::io::Error },
+    #[snafu(display("failed to open cache file: {}", source))]
+    FileOpen { source: std::io::Error },
+    #[snafu(display("serde failed to serialize: {}", source))]
+    Serialization { source: serde_json::Error },
+    #[snafu(display("serde failed to deserialize: {}", source))]
+    Deserialization { source: serde_json::Error },
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct ModCache {
@@ -18,6 +34,31 @@ impl ModCache {
             version: 1,
             mods: HashSet::new(),
         }
+    }
+
+    pub fn from_disk_or_empty(repo_path: &Path) -> Result<Self, Error> {
+        let path = repo_path.join("nimble-cache.json");
+        let open_result = File::open(path);
+        match open_result {
+            Ok(file) => {
+                let reader = BufReader::new(file);
+                serde_json::from_reader(reader).context(DeserializationSnafu)
+            }
+            Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
+                Ok(Self::new_empty())
+            },
+            Err(e) => Err(Error::FileOpen { source: e })
+        }
+    }
+
+    pub fn to_disk(&self, repo_path: &Path) -> Result<(), Error> {
+        let path = repo_path.join("nimble-cache.json");
+        let file = File::create(path).context(FileCreationSnafu)?;
+        let writer = BufWriter::new(file);
+
+        serde_json::to_writer(writer, &self).context(SerializationSnafu)?;
+
+        Ok(())
     }
 
     pub fn update_mod_checksum(&mut self, old_checksum: &Md5Digest, new_checksum: Md5Digest) {
