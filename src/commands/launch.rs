cc078dd7e2f03108f601c29990e3350fb2419a0e
Victor Chiletto <v@hnn.net.br>
2022-11-23 00:26:13 -0300

    more launch work
    steam protocol is horribly unreliable. might have to launch game directly.

--- src/commands/launch.rs
+++ src/commands/launch.rs
@@ -1,51 +1,66 @@
 use crate::mod_cache;
 use crate::mod_cache::ModCache;
-use snafu::{ResultExt, Snafu};
-use std::path::{PathBuf, Path};
+use snafu::{OptionExt, ResultExt, Snafu};
 use std::cfg;
+use std::path::{Path, PathBuf};
 
 #[derive(Debug, Snafu)]
 pub enum Error {
     #[snafu(display("failed to open ModCache: {}", source))]
     ModCacheOpen { source: mod_cache::Error },
+    #[snafu(display("failed to find drive_c"))]
+    FailedToFindDriveC,
 }
 
 fn generate_mod_args(base_path: &Path, mod_cache: &ModCache) -> String {
-    mod_cache.mods.values().fold(String::from("-mod="), |acc, r#mod| {
-        let mod_name = &r#mod.name;
-        let full_path = base_path.join(Path::new(mod_name)).to_string_lossy().to_string();
-        format!("{acc}{full_path};")
-    })
+    mod_cache
+        .mods
+        .values()
+        .fold(String::from("-noLauncher -mod="), |acc, r#mod| {
+            let mod_name = &r#mod.name;
+            let full_path = base_path
+                .join(Path::new(mod_name))
+                .to_string_lossy()
+                .to_string();
+            format!("{acc}{full_path};")
+        })
 }
 
 // if we're on windows we don't have to do anything
 #[cfg(windows)]
-fn convert_host_base_path_to_proton_base_path(host_base_path: PathBuf) -> PathBuf {
-    host_base_path
+fn convert_host_base_path_to_proton_base_path(host_base_path: &Path) -> Result<PathBuf, Error> {
+    Ok(host_base_path.to_owned())
 }
 
 // if we're not on windows, try to find a "drive_c" dir in the ancestors of base_path
 #[cfg(not(windows))]
-fn convert_host_base_path_to_proton_base_path(host_base_path: PathBuf) -> PathBuf {
-    let drive_c_path = host_base_path.ancestors().find(|&x| x.ends_with("drive_c")).unwrap();
+fn convert_host_base_path_to_proton_base_path(host_base_path: &Path) -> Result<PathBuf, Error> {
+    let drive_c_path = host_base_path
+        .ancestors()
+        .find(|&x| x.ends_with("drive_c"))
+        .context(FailedToFindDriveCSnafu)?;
 
-    let relative = host_base_path.strip_prefix(drive_c_path).unwrap();
+    let relative = host_base_path
+        .strip_prefix(drive_c_path)
+        .expect("drive_c_path was not a prefix of host_base_path, this should never happen");
 
-    Path::new("c:/").join(relative)
+    Ok(Path::new("c:/").join(relative))
 }
 
 pub fn launch(base_path: PathBuf) -> Result<(), Error> {
     let mod_cache = ModCache::from_disk_or_empty(&base_path).context(ModCacheOpenSnafu)?;
 
-    let proton_base_path = convert_host_base_path_to_proton_base_path(base_path);
+    let proton_base_path = convert_host_base_path_to_proton_base_path(&base_path)?;
 
-    let cmdline = mod_cache.mods.values().fold(String::from("-mod="), |acc, r#mod| {
-        let mod_name = &r#mod.name;
-        let full_path = proton_base_path.join(Path::new(mod_name)).to_string_lossy().to_string();
-        format!("{acc}{full_path};")
-    });
+    let binding = generate_mod_args(&proton_base_path, &mod_cache);
+    let cmdline =
+        percent_encoding::utf8_percent_encode(&binding, percent_encoding::NON_ALPHANUMERIC);
 
-    dbg!(cmdline);
+    let steam_url = format!("steam://run/107410//{cmdline}/");
+
+    dbg!(&steam_url);
+
+    open::that(steam_url).unwrap();
 
     Ok(())
 }
@@ -59,7 +74,7 @@ mod tests {
     fn test_proton_path_conversion() {
         // on windows, this should do nothing
         let original_path = PathBuf::from("C:\\random\\paths\\drive_c\\banana_repo");
-        let converted = convert_host_base_path_to_proton_base_path(original_path.clone());
+        let converted = convert_host_base_path_to_proton_base_path(&original_path).unwrap();
 
         assert_eq!(original_path, converted);
     }
@@ -69,7 +84,7 @@ mod tests {
     fn test_proton_path_conversion() {
         // on windows, this should do nothing
         let original_path = PathBuf::from("/home/random/paths/drive_c/banana_repo");
-        let converted = convert_host_base_path_to_proton_base_path(original_path.clone());
+        let converted = convert_host_base_path_to_proton_base_path(&original_path).unwrap();
 
         assert_eq!(converted, PathBuf::from("c:/banana_repo"));
     }
