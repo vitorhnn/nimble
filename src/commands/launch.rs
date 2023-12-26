4adb567b262ac04cd9e0748e15f4e1c5ffc824ff
Victor Chiletto <v@hnn.net.br>
2023-12-25 23:03:04 -0300

    wip launch trash
    there's a pretty big chance this is actually completely useless as it
    seems valve has fixed steam urls, hopefully, maybe.

--- src/commands/launch.rs
+++ src/commands/launch.rs
@@ -3,13 +3,24 @@ use crate::mod_cache::ModCache;
 use snafu::{OptionExt, ResultExt, Snafu};
 use std::cfg;
 use std::path::{Path, PathBuf};
+use steamlocate::{SteamDir, CompatTool};
+
+const ARMA3_APPID: u32 = 107410;
 
 #[derive(Debug, Snafu)]
 pub enum Error {
     #[snafu(display("failed to open ModCache: {}", source))]
     ModCacheOpen { source: mod_cache::Error },
+    #[snafu(display("steamlocate error: {}", source))]
+    SteamLocate { source: steamlocate::Error },
     #[snafu(display("failed to find drive_c"))]
     FailedToFindDriveC,
+    #[snafu(display("couldn't find arma"))]
+    FailedToFindArma,
+    #[snafu(display("read dir IO error: {}", source))]
+    ReadDir { source: std::io::Error },
+    #[snafu(display("couldn't find compat tool"))]
+    CompatToolNotFound,
 }
 
 fn generate_mod_args(base_path: &Path, mod_cache: &ModCache) -> String {
@@ -47,11 +58,68 @@ fn convert_host_base_path_to_proton_base_path(host_base_path: &Path) -> Result<P
     Ok(Path::new("c:/").join(relative))
 }
 
+pub fn find_compat_tool(steam_dir: &SteamDir, compat_tool: &CompatTool) -> Result<PathBuf, Error> {
+    // try to find it in steam compatibilitytools.d, if not found, try to find it in any library
+    let steam_path = steam_dir.path();
+    let compat_tool_name = compat_tool.name.as_ref().expect("no compat tool name");
+    let mut compat_tool_path = steam_path.join("compatibilitytools.d");
+    compat_tool_path.push(compat_tool_name);
+
+    if compat_tool_path.exists() {
+        return Ok(compat_tool_path);
+    }
+
+    // we have to do a weird scan of all steam libraries.
+    // a lot of this is adapted from github.com/muttleyxd/arma3-unix-launcher
+    if let Some((library, app)) = 'found: {
+        for library in steam_dir
+            .libraries()
+            .context(SteamLocateSnafu)?
+            .filter_map(Result::ok) {
+            for app in library.apps().filter_map(Result::ok) {
+                if app.name.as_ref().is_some_and(|name| name == compat_tool_name) {
+                    break 'found Some((library, app));
+                }
+            }
+        }
+        None
+    } {
+        Ok(library.resolve_app_dir(&app))
+    } else {
+        Err(Error::CompatToolNotFound)
+    }
+}
+
+pub fn launch_directly(arma_path: &Path) {
+    todo!();
+}
+
+pub fn launch_compat_tool(arma_path: &Path, compat_tool_path: &Path) {
+    todo!();
+}
+
 pub fn launch(base_path: PathBuf) -> Result<(), Error> {
     let mod_cache = ModCache::from_disk_or_empty(&base_path).context(ModCacheOpenSnafu)?;
 
     let proton_base_path = convert_host_base_path_to_proton_base_path(&base_path)?;
 
+    let steam_dir = steamlocate::SteamDir::locate().context(SteamLocateSnafu)?;
+    if let Some((arma, library)) = steam_dir.find_app(ARMA3_APPID).context(SteamLocateSnafu)? {
+        let arma_path = library.resolve_app_dir(&arma);
+        if let Some(compat_tool) = steam_dir.compat_tool_mapping().context(SteamLocateSnafu)?.get(&ARMA3_APPID) {
+            let compat_tool_path = find_compat_tool(&steam_dir, &compat_tool)?;
+            dbg!("compat tool is {}, path {}", compat_tool, compat_tool_path);
+            todo!();
+            launch_compat_tool(&arma_path, &compat_tool_path);
+        } else {
+            launch_directly(&arma_path);
+        }
+
+        Ok(())
+    } else {
+        Err(Error::FailedToFindArma)
+    }
+    /*
     let binding = generate_mod_args(&proton_base_path, &mod_cache);
     let cmdline =
         percent_encoding::utf8_percent_encode(&binding, percent_encoding::NON_ALPHANUMERIC);
@@ -63,6 +131,7 @@ pub fn launch(base_path: PathBuf) -> Result<(), Error> {
     open::that(steam_url).unwrap();
 
     Ok(())
+     */
 }
 
 #[cfg(test)]
